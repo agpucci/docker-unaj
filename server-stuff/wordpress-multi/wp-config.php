9e6e0c723fa6167ffcca4d65ddff720eb6009114
Agustin Pucci <agpucci@gmail.com>
2024-08-20 18:32:24 -0300

    Ajustes

--- server-stuff/wordpress-multi/wp-config.php
+++ server-stuff/wordpress-multi/wp-config.php
@@ -84,7 +84,13 @@ define( 'WP_DEBUG', false );
 /* Add any custom values between this line and the "stop editing" line. */
 
 
-
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', 'localhost:8080' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
